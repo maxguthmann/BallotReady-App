69f2e0351ae91850effa629c0d3a1719be9af822
unknown <maxguthmann11@gmail.com>
2018-05-17 12:06:10 -0500

    commit

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ app= Flask(__name__)
 
 @app.route('/')
 def hello():
-	#conn=psycopg2.connect(host='db', user='postgres')
 	engine=create_engine('postgresql://postgres:password@db:5432')
 	db = engine.connect()
 	db.execute("CREATE TABLE IF NOT EXISTS tab(col INTEGER)")
@@ -15,15 +14,6 @@ def hello():
 	for r in result:
 		return str(r)
 	db.close()
-	#cur=conn.cursor()
-	#cur.execute("CREATE TABLE IF NOT EXISTS tab(col INTEGER)")
-	#cur.execute("INSERT INTO tab(col) VALUES(1)")
-	#conn.commit()
-	#cur.execute("SELECT * FROM tab")
-	#result=cur.fetchone()
-	
-
-
 
 app.run(host="0.0.0.0")
 
